fe8408cda117afd4e29bda9c3e638b5120f7440d
zryan2 <zeng.ryan2@gmail.com>
2019-08-07 14:00:09 -0400

    reverted database changes

--- src/main/java/com/babydragons/oneride/oneride/config/OneRideConfig.java
+++ src/main/java/com/babydragons/oneride/oneride/config/OneRideConfig.java
@@ -1,13 +1,11 @@
 package com.babydragons.oneride.oneride.config;
 
-import com.babydragons.database.config.PostgresDatabaseConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan(basePackages = "com.babydragons.*")
-@Import({PostgresDatabaseConfig.class})
+//@Import({PostgresDatabaseConfig.class})
 public class OneRideConfig implements WebMvcConfigurer {
 }
--- src/main/java/com/babydragons/oneride/oneride/controllers/OneRideController.java
+++ src/main/java/com/babydragons/oneride/oneride/controllers/OneRideController.java
@@ -1,7 +1,5 @@
 package com.babydragons.oneride.oneride.controllers;
 
-import com.babydragons.database.dao.UserRepository;
-import com.babydragons.database.entities.UserEntity;
 import com.babydragons.oneride.oneride.models.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,32 +13,32 @@ import java.util.List;
 @RestController
 @RequestMapping("/oneride")
 public class OneRideController {
-
-    @Autowired
-    private UserRepository userRepository;
+//
+//    @Autowired
+//    private UserRepository userRepository;
 
     @GetMapping
     public String test(){
         return "test";
     }
-
-    @GetMapping("/users")
-    public List<UserResponse> getResources(){
-        Iterable<UserEntity> users = userRepository.findAll();
-        List<UserResponse> responses = new ArrayList<>();
-        for(UserEntity user : users){
-            responses.add(new UserResponse(user.getId(), user.getUsername()));
-        }
-        return responses;
-    }
-
-
-
-    @PostMapping("/users")
-    public void saveData(){
-        userRepository.save(new UserEntity(1L, "test"));
-        userRepository.save(new UserEntity(2L, "test"));
-
-    }
+//
+//    @GetMapping("/users")
+//    public List<UserResponse> getResources(){
+//        Iterable<UserEntity> users = userRepository.findAll();
+//        List<UserResponse> responses = new ArrayList<>();
+//        for(UserEntity user : users){
+//            responses.add(new UserResponse(user.getId(), user.getUsername()));
+//        }
+//        return responses;
+//    }
+//
+//
+//
+//    @PostMapping("/users")
+//    public void saveData(){
+//        userRepository.save(new UserEntity(1L, "test"));
+//        userRepository.save(new UserEntity(2L, "test"));
+//
+//    }
 
 }
