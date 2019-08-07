01a5f0c25637d6b44412a550c383a957accfb197
zryan2 <zeng.ryan2@gmail.com>
2019-08-07 11:08:34 -0400

    initial commit

---
+++ src/main/java/com/babydragons/oneride/oneride/OnerideApplication.java
@@ -0,0 +1,13 @@
+package com.babydragons.oneride.oneride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnerideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnerideApplication.class, args);
+    }
+
+}
